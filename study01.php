e2d59300e351a5b38d88caff216e90b2941cc992
inotaka <47511254+inotaka@users.noreply.github.com>
2020-05-21 12:40:56 +0900

    Add files via upload

---
+++ study01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>study01.php</title>
+</head>
+<body>
+
+<?php
+    $a = 2;
+    $b = 3;
+    
+    echo $c = $a + $b;
+?>
+
+</body>
+</html>
